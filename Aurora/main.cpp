a687915efb60518a8b27300a6dbe68df0664ff43
Espen Nordahl <espen.nordahl@gmail.com>
2012-06-30 10:39:01 +1200

    Initial Commit

---
+++ Aurora/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Aurora
+//
+//  Created by Espen Nordahl on 30/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
